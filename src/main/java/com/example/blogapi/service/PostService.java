27f10a440e14e7082aa1e34be3aa47b2b4a25c52
Jesse Murah <95482112+JesseMurah@users.noreply.github.com>
2024-11-07 18:35:27 +0000

    Implement post crud operations in the post service class (#5)
    * added User and Post entities in model package
    
    * added repositories for User and Posts
    
    * added service class to the user entity
    
    * added service class to the post entity
    
    * added endpoints to perform post crud operations
    
    * added others in the project
    
    * Implemented post CRUD functionality

--- src/main/java/com/example/blogapi/service/PostService.java
+++ src/main/java/com/example/blogapi/service/PostService.java
@@ -8,6 +8,7 @@ import com.example.blogapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.Optional;
 
 @Service
